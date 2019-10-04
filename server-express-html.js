e8ad3f40bc370fcbefc6d2f8af8c8e98d2cde01e
sebastian <sebastiano-10@hotmail.com>
2019-10-03 21:20:47 -0500

    cambios

--- server-express-html.js
+++ server-express-html.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
 const port = process.env.port || 3000
@@ -6,4 +6,4 @@ const port = process.env.port || 3000
 app.use(express.static(__dirname + '/public_html'))  
 app.listen(port)
 
-console.log(`Server en el puerto ${port}`);
+//console.log(`Server en el puerto ${port}`);
